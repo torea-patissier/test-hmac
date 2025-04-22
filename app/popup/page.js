0db25aa19ed9f0e5be464f41a5111d182601f60b
Torea <p.torea@icloud.com>
2025-04-22 12:40:27 +0400

    x

--- app/popup/page.js
+++ app/popup/page.js
@@ -4,7 +4,7 @@ import PopupTest from "@/components/PopupTest";
 export default function PopupPage() {
   return (
     <>
-            <h1 className="text-4xl text-center p-5 font-bold text-gray-800 mb-8">Projet A</h1>
+    <h1 className="text-4xl text-center p-5 font-bold text-gray-800 mb-8">Projet A</h1>
         <div className="flex items-center justify-center min-h-screen">
             <PopupTest />
         </div>
--- public/churn.js
+++ public/churn.js
@@ -16,22 +16,6 @@
         iframe.src = `${baseUrl}/popup`;
         iframe.allowTransparency = "true"; // 👈 nécessaire sur certains navigateurs
         iframe.id = "churn-popup-iframe";
-        iframe.style = `
-          position: fixed;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          width: 500px;
-          max-width: 90%;
-          height: auto;
-          min-height: 300px;
-          max-height: 90vh;
-          border: none;
-          border-radius: 8px;
-          background: white;
-          box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-          z-index: 9999;
-        `;
         document.body.appendChild(iframe);
       }
     };
