db6f8014a789745e4a79337aaf2647445da4eddf
Torea <p.torea@icloud.com>
2025-04-22 12:25:02 +0400

    x

--- app/popup/page.js
+++ app/popup/page.js
@@ -4,7 +4,6 @@ import PopupTest from "@/components/PopupTest";
 export default function PopupPage() {
   return (
     <>
-        <h1>Hello</h1>
     <div className="flex items-center justify-center min-h-screen">
       <PopupTest />
     </div>
